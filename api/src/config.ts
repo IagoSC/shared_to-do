eb2f1c8b765af4ffab8e96cec0f8a1773f667821
Iago Sousa <iagosousavix@gmail.com>
2023-06-03 19:22:29 -0300

    Improve config.ts

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,3 +1,10 @@
-export const config = {
-  HTTP_PORT: Number(process.env.HTTP_PORT || 80),
+const config = {
+  // API
+  httpPort: Number(process.env.HTTP_PORT || 80),
+
+  // DATABASE
+  dbHost: process.env.DATABASE_HOST || "localhost",
+  dbPort: Number(process.env.DATABASE_PORT || 5432),
 };
+
+export { config };
